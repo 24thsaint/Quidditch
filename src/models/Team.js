6197ecf6a91b4d170bdeabd50be36ff6abd5b679
24thsaint <admin@blackout.biz.tm>
2016-11-07 01:36:50 +0800

    Models: Added 'Team' model

---
+++ src/models/Team.js
@@ -0,0 +1,32 @@
+import mongoose from 'mongoose'
+import Model from './Model'
+
+const schema = mongoose.Schema({
+  name: { type: String },
+  players: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Player' }],
+})
+
+class Team extends Model {
+
+  addPlayer(player) {
+    this.players.push(player)
+  }
+
+  get score() {
+    let goalsMade = 0
+    let score = 0
+    for (const player of this.players) {
+      goalsMade += player.goals
+      if (player.snitchCaught) {
+        score += 30 // https://www.usquidditch.org/about/rules
+      }
+    }
+    score += goalsMade * 10
+    this.score = score
+    return score
+  }
+
+}
+
+// export default Team
+export default Model.load('Team', Team, schema)
