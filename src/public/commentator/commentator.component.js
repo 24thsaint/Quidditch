0f6a410b0d4ea3a2026240b1c9f0617e1e306145
24thsaint <admin@blackout.biz.tm>
2016-11-08 17:16:03 +0800

    Fix: Frontend Renders

--- src/public/commentator/commentator.component.js
+++ src/public/commentator/commentator.component.js
@@ -1,35 +1,41 @@
 /* eslint-disable no-param-reassign */
 
 angular // eslint-disable-line
-  .module('commentator')
-  .component('commentator', {
-    templateUrl: '/templates/commentator-template.html',
-    controller: ['$routeParams', '$http', '$scope',
-      function CommentatorController($routeParams, $http, $scope) {
-        const gameId = $routeParams.gameId
-        $http.get(`/game/find/${gameId}`)
-          .success((data) => {
-            $scope.game = data
-            console.log(data)
-          })
-          .error((data) => {
-            console.log(`ERROR: ${data}`) // eslint-disable-line
-          })
-        $scope.goal = function goal() {
-          alert('Goal')
-        }
-        $scope.miss = function miss() {
-          alert('Miss')
-        }
-        $scope.block = function block() {
-          alert('Block')
-        }
-        $scope.snitchAppeared = function snitchAppeared() {
-          alert('Appeared')
-        }
-        $scope.end = function end() {
-          alert('End')
-        }
-      },
-    ],
-  })
+.module('commentator')
+.component('commentator', {
+  templateUrl: '/templates/commentator-template.html',
+  controller: ['$routeParams', '$http', '$scope',
+    ($routeParams, $http, $scope) => {
+      const gameId = $routeParams.gameId
+      $http.get(`/game/find/${gameId}`)
+    .success((data) => {
+      $scope.game = data
+      $scope.teams = []
+      for (const team of data.teams) {
+        $http.get(`/team/${team._id}`) // eslint-disable-line
+        .success((teamData) => {
+          $scope.teams.push(teamData)
+        })
+      }
+    })
+    .error((data) => {
+      console.log(`ERROR: ${data}`) // eslint-disable-line
+    })
+      $scope.goal = () => {
+        console.log('Goal')
+      }
+      $scope.miss = () => {
+        console.log('Miss')
+      }
+      $scope.block = () => {
+        console.log('Block')
+      }
+      $scope.snitchAppeared = () => {
+        console.log('Appeared')
+      }
+      $scope.end = function end() {
+        console.log('End')
+      }
+    },
+  ],
+})
